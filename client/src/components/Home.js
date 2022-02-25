2e596164064e8698a753d82bed96b268815bb883
thomasalemayehu <61502533+thomasalemayehu@users.noreply.github.com>
2022-02-25 16:10:57 +0300

    Home Hero Complete

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,13 @@
 import "../styles/Home.css";
 import ScienceOutlinedIcon from "@mui/icons-material/ScienceOutlined";
 import PreviewOutlinedIcon from "@mui/icons-material/PreviewOutlined";
+import SchoolOutlinedIcon from "@mui/icons-material/SchoolOutlined";
 
 function Home() {
   return (
     <div className="home__root">
       <div className="home__hero">
+        <SchoolOutlinedIcon className="home__hero__icon" />
         <h1 className="home__display">College Tracker</h1>
         <h3 className="home__description">Find the University for you</h3>
         <div className="home__options__container">
